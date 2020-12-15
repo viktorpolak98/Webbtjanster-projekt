4bca232efa685759008672b86b99b3b534962b56
viktorpolak98 <61971346+viktorpolak98@users.noreply.github.com>
2020-12-15 14:36:33 +0100

    Add files via upload

--- ApiObject.java
+++ ApiObject.java
@@ -6,42 +6,43 @@ public class ApiObject {
     private String url;
     private String type;
     private String location;
-}
 
-public ApiObject(String id, String datetime, String name, String summary, String url, String type, String location) {
-    this.id = id;
-    this.datetime = datetime;
-    this.name = name;
-    this.summary = summary;
-    this.url = url;
-    this.type = type;
-    this.location = location;
-}
+    public ApiObject(String id, String datetime, String name, String summary, String url, String type, String location) {
+        this.id = id;
+        this.datetime = datetime;
+        this.name = name;
+        this.summary = summary;
+        this.url = url;
+        this.type = type;
+        this.location = location;
+    }
 
-public String getId() {
-    return this.id;
-}
+    public String getId() {
+        return this.id;
+    }
 
-public String getDatetime() {
-    return this.datetime;
-}
+    public String getDatetime() {
+        return this.datetime;
+    }
 
-public String getName() {
-    return this.name;
-}
+    public String getName() {
+        return this.name;
+    }
 
-public String getSummary() {
-    return this.summary;
-}
+    public String getSummary() {
+        return this.summary;
+    }
 
-public String getUrl() {
-    return this.url;
-}
+    public String getUrl() {
+        return this.url;
+    }
 
-public String getType() {
-    return this.type;
-}
+    public String getType() {
+        return this.type;
+    }
 
-public String getLocation() {
-    return this.location;
+    public String getLocation() {
+        return this.location;
+    }
+
 }
